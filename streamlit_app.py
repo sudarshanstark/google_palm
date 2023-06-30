49e90bd82cb45e0c66e4eac7860acec8f84a7ca8
suda_r <38130790+sudarshanstark@users.noreply.github.com>
2023-06-30 20:54:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pprint
 import google.generativeai as palm
 
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+
 palm.configure(api_key='AIzaSyDeXnoB50k2ULQ_zWXkOytKwpDTdFezo1I')
 
 prompt = st.text_input('Enter your prompt here:')
